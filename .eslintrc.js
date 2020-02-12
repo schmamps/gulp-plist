19f428cf424d03119e6849224aaf4eeec8feb9b9
Andrew Champion <awchampion@gmail.com>
2020-02-11 16:04:48 -0800

    add mocha globals updates for code style

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,9 @@ module.exports = {
         "node": true,
 	},
 	"globals": {
+		"context": true,
 		"describe": true,
+		"it": true,
 	},
     "extends": "eslint:recommended",
     "rules": {
@@ -12,7 +14,7 @@ module.exports = {
         "array-bracket-newline": "error",
         "array-bracket-spacing": "error",
         "array-callback-return": "error",
-        "array-element-newline": "error",
+        "array-element-newline": "off",
         "arrow-body-style": "error",
         "arrow-parens": "error",
         "arrow-spacing": "error",
